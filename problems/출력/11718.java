c9b3abec271d38c10a09779429657ee8d767a2ce
live2skull <live2skull@gmail.com>
2020-03-31 22:50:50 +0900

    recommet untracked file

---
+++ problems/출력/11718.java
@@ -0,0 +1,23 @@
+package kr.live2skull.algorithm;
+
+import java.util.*;
+import java.io.*;
+
+// 11718 그대로 출력하기 - 출력
+// 2020. 03. 31
+
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext())
+        {
+            // sc.hasNext() => EOF 를 입력받기 전까지 실행합니다.
+            System.out.println(sc.nextLine());
+        }
+    }
+}
